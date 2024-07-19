b47e5c8940e92478a9c9a25f749fa65e35ba0c5f
sonusatish34 <71165289+sonusatish34@users.noreply.github.com>
2024-07-19 16:27:40 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // reactStrictMode: true,
-  distDir: "build",
-  output: "export",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'd10uth61hedy2t.cloudfront.net',
-        
       },
     ],
   },
