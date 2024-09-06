b74b927ccf998f5516bbfb414007d3956b16a582
RahulRajesh <rahulrajesheerla@gmail.com>
2024-09-06 19:00:04 +0530

    hi  rajesh

--- pages/self-drive-car-rental/[place].js
+++ pages/self-drive-car-rental/[place].js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
+import patancheru from '../components/Branches/Patancheru';
+import Mehdipatnam from '../components/Branches/mehdipatnam';
+import mehdipatnam from '../components/Branches/mehdipatnam';
+import khajaguda from '../components/Branches/khajaguda';
+import nanakramguda from '../components/Branches/nanakramguda';
+import hitech from '../components/Branches/hitech';
+import lbnagar from '../components/Branches/lbnagar';
 
 const components = {
   ameerpet: dynamic(() => import('../components/Branches/ameerpet'), { ssr: false }),
@@ -14,6 +21,15 @@ const components = {
   shamshabad: dynamic(() => import('../components/Branches/shamshabad')),
   ramanthapur: dynamic(() => import('../components/Branches/ramanthapur')),
   gachibowli: dynamic(() => import('../components/Branches/gachibowli')),
+  patancheru: dynamic(() => import('../components/Branches/patancheru')),
+  mehdipatnam: dynamic(() => import('../components/Branches/mehdipatnam')),
+  hitech: dynamic(() => import('../components/Branches/hitech')),
+  khajaguda: dynamic(() => import('../components/Branches/khajaguda')),
+  lbnagar:dynamic(()=>import('../components/Branches/lbnagar')),
+ nanakramguda: dynamic(() => import('../components/Branches/nanakramguda')),
+
+
+
 };
 
 function Place() {
